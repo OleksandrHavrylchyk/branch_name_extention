1501b495c6a1421e0eeb36c63a94b8c232b561b7
OleksanrHavrylchyk <39791219+OleksandrHavrylchyk@users.noreply.github.com>
2020-07-09 16:33:40 +0300

    Upload files

---
+++ branch_namer/menu.js
@@ -0,0 +1,31 @@
+let button = document.getElementById('submitButton');
+
+button.onclick = function() {
+    formatAndCopy();
+};
+
+function formatAndCopy() {
+    let input = document.getElementById('taskName');
+    input.value = formatString(input.value);
+
+    input.select();
+    document.execCommand("copy");
+}
+
+function formatString(str) {
+    let formatedString = str.replace(':', '')
+                            .replace(/[^\w\s]/gi, '_')
+                            .split(' ')
+                            .join('_')
+                            .toLowerCase();
+
+    if(formatedString.startsWith('task')) {
+        return 'feature/' + formatedString.replace('task_', 'task-');       
+    }
+
+    if(formatedString.startsWith('bug')) {
+        return 'fix/' + formatedString.replace('bug_', 'bug-');
+    }
+    
+    return str
+}
